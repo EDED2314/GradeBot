bc246cfd5b423a73df9a9b2e91a9c803a87b23db
ed.xyz <91804938+EDED2314@users.noreply.github.com>
2022-05-25 21:17:05 -0400

    Update Grades.py

--- GradeBot/Grades.py
+++ GradeBot/Grades.py
@@ -139,7 +139,7 @@ class get_the_grade_from_website:
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=option)
         driver.implicitly_wait(30)
         # print("going to website")
-        driver.get("https://parents.mtsd.k12.nj.us/genesis/sis")
+        driver.get("website link")
         # print("finding username")
         username = driver.find_element(By.ID, "j_username").send_keys(f"{email}")
         password = driver.find_element(By.ID, "j_password").send_keys(f"{password}")
@@ -168,45 +168,6 @@ class get_the_grade_from_website:
         data = thing.get_data(soup)
         grades = thing.gen_data(data)
         return (info, grades, courses, view)
-
-
-
-class Dropdown(disnake.ui.Select):
-    def __init__(self):
-
-        # Set the options that will be presented inside the dropdown
-        options = [
-            disnake.SelectOption(
-                label="Red", description="Your favourite colour is red", emoji="🟥"
-            ),
-            disnake.SelectOption(
-                label="Green", description="Your favourite colour is green", emoji="🟩"
-            ),
-            disnake.SelectOption(
-                label="Blue", description="Your favourite colour is blue", emoji="🟦"
-            ),
-        ]
-
-        # The placeholder is what will be shown when no option is chosen
-        # The min and max values indicate we can only pick one of the three options
-        # The options parameter defines the dropdown options. We defined this above
-        super().__init__(
-            placeholder="Choose what account you want",
-            min_values=1,
-            max_values=1,
-            options=options,
-        )
-
-    async def callback(self, interaction: disnake.MessageInteraction):
-        await interaction.response.send_message(f"Your favourite colour is {self.values[0]}")
-
-
-class View(disnake.ui.View):
-    def __init__(self):
-        super().__init__()
-
-        # Adds the dropdown to our view object.
-        self.add_item(Dropdown())
 
 
 class GetGrade(commands.Cog):
